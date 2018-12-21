cb7acdce81fd2bbd6dbe62bd3606dd3362d2135f
Rahul Gaur <rahul.gaur152@gmail.com>
2018-12-21 08:14:42 +0530

    added layouts and removed fragments

--- app/src/main/java/in/rahulgaur/parkedfind/Register.java
+++ app/src/main/java/in/rahulgaur/parkedfind/Register.java
@@ -1,31 +1,15 @@
 package in.rahulgaur.parkedfind;
 
-
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class Register extends Fragment {
-
-
-    public Register() {
-        // Required empty public constructor
-    }
-
+public class Register extends AppCompatActivity {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
-
 }
--- app/src/main/java/in/rahulgaur/parkedfind/Welcome.java
+++ app/src/main/java/in/rahulgaur/parkedfind/Welcome.java
@@ -1,8 +1,11 @@
 package in.rahulgaur.parkedfind;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
     }
 }
